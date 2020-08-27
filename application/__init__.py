e55f2a4186bb9049b0f52e760553b36825fa8ca9
Artem Dereviago <54147072+rupeq@users.noreply.github.com>
2020-08-27 18:00:31 +0300

    Add files via upload

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,17 @@
 from flask import Flask
+from flask_mongoengine import MongoEngine
+from flask_restful import Api
+
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+api = Api(app)
+
+db = MongoEngine()
+db.init_app(app)
 
 from application import routes
+
+api.add_resource(routes.GetPostAPI, '/api/')
+api.add_resource(routes.GetUpdateDeleteAPI, '/api/<idx>/')
